fa4377e9fc08ba902b598f767df092f3a563c7e9
Brad Miller <bonelake@mac.com>
2016-03-02 09:21:43 -0600

    Stack based method of making permutations

---
+++ perm.py
@@ -0,0 +1,37 @@
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+def perms(word):
+    stack = Stack()
+    for l in word:
+        stack.push(l)
+    results = [stack.pop()]
+    while stack.size() != 0:
+        c = stack.pop()
+        new_results = []
+        for w in results:
+            for i in range(len(w)+1):
+                new_results.append(w[:i] + c + w[i:])
+        results = new_results
+    return results
+
+
+print(perms("lut"))
