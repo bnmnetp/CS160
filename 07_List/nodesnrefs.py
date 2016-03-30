709cdd74be16680364a6c50a78f7a996c6edef43
Brad Miller <bonelake@mac.com>
2016-03-30 10:17:23 -0500

    timing comparisons

--- 07_List/nodesnrefs.py
+++ 07_List/nodesnrefs.py
@@ -9,7 +9,7 @@ class UnorderedList:
     def __init__(self):
         self.head = None
 
-    def add(self, payload):
+    def append(self, payload):
         if self.head == None:
             self.head = Node(payload)
         else:
@@ -19,11 +19,20 @@ class UnorderedList:
 
             currentNode.next = Node(payload)
 
+    def add(self, payload):
+        if self.head == None:
+            self.head = Node(payload)
+        else:
+            newNode = Node(payload)
+            newNode.next = self.head
+            self.head = newNode
+
     def print(self):
         currentNode = self.head
         while currentNode != None:
             print(currentNode.data)
             currentNode = currentNode.next
+
 
     def remove(self,item):
         # find the node where the payload matches item
@@ -46,15 +55,63 @@ class UnorderedList:
                 currentNode = currentNode.next
 
 
+    def __getitem__(self, item):
+        currentNode = self.head
+        idx = 0
+        while currentNode != None and idx < item:
+            currentNode = currentNode.next
+            idx += 1
+
+        if idx == item:
+            return currentNode.data
+        else:
+            raise IndexError("list index out of range")
+
+
+    def __setitem__(self, setidx, val):
+        currentNode = self.head
+        idx = 0
+        while currentNode != None and idx < setidx:
+            currentNode = currentNode.next
+            idx += 1
+
+        if idx == setidx and currentNode:
+            currentNode.data = val
+        else:
+            raise IndexError("list index out of range")
+
 mylist = UnorderedList()
 for i in range(10):
-    mylist.add(i)
+    mylist.append(i)
 
 mylist.print()
-
+print("====")
 mylist.remove(5)
-
 mylist.print()
-mylist.remove(0)
+print("===")
+print("testing index", mylist[3])
+#print(mylist[20])
+print(mylist[8])
+mylist.print()
 
 
+
+
+# ==============
+
+from timeit import Timer
+
+def test1():
+    l = UnorderedList()
+    for i in range(1000):
+        l.add(i)
+
+def test2():
+    l = list()
+    for i in range(1000):
+        l.insert(0,i)
+
+t1 = Timer("test1()", "from __main__ import test1")
+print(t1.timeit(1000))
+t2 = Timer("test2()", "from __main__ import test2")
+print(t2.timeit(1000))
