1a1ba90d3681bc19dc929867106c978711f1d1ee
Brad Miller <bonelake@mac.com>
2016-05-05 07:29:32 -0500

    use groupby

--- 13_KevinBacon/kevinbacon.py
+++ 13_KevinBacon/kevinbacon.py
@@ -1,23 +1,34 @@
 from pythonds import Graph, Queue
 import sys
-
+from itertools import groupby
 
 baconGraph = Graph()
 
+# with open('movie_actors.csv') as cast:
+#     prev_movie, actor = cast.readline().strip().split('|')
+#     alist = [actor]
+#     for line in cast:
+#         movie, actor = line.strip().split('|')
+#         if movie == prev_movie:
+#             alist.append(actor)
+#         else:
+#             for i in alist:
+#                 for j in alist:
+#                     if i != j:
+#                         baconGraph.addEdge(i,j,prev_movie)
+#             prev_movie = movie
+#             alist = [actor]
+
+# this is kind of a cool way to do this with the itertools.groupby function.
+
 with open('movie_actors.csv') as cast:
-    prev_movie, actor = cast.readline().strip().split('|')
-    alist = [actor]
-    for line in cast:
-        movie, actor = line.strip().split('|')
-        if movie == prev_movie:
-            alist.append(actor)
-        else:
-            for i in alist:
-                for j in alist:
-                    if i != j:
-                        baconGraph.addEdge(i,j,prev_movie)
-            prev_movie = movie
-            alist = [actor]
+    groups = groupby(cast, key=lambda x: x.strip().split('|')[0])
+    for movie, group in groups:
+        actors = [x.strip().split('|')[1] for x in group]
+        for i in actors:
+            for j in actors:
+                if i != j:
+                    baconGraph.addEdge(i,j,movie)
 
 kevin = baconGraph.getVertex('Kevin Bacon')
 for n in kevin.connectedTo:
